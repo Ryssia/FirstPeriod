cb5c1130ef84b85777f2d576d9be314b656742ec
Ryssia.Elias <68912012+Ryssia@users.noreply.github.com>
2023-01-17 22:30:44 -0300

    finalizado repositório Usuário

--- src/main/java/ifpr/pgua/eic/projetointegrador/model/repositories/UsuarioRepository.java
+++ src/main/java/ifpr/pgua/eic/projetointegrador/model/repositories/UsuarioRepository.java
@@ -9,37 +9,46 @@ import ifpr.pgua.eic.projetointegrador.model.results.Result;
 
 public class UsuarioRepository {
 
-    private UsuarioDAO dao;
+    private UsuarioDAO dao; //DAO Data Access Object
 
     public UsuarioRepository(UsuarioDAO dao) {
         this.dao = dao;
     }
 
-    // public Result salvar(Usuario usuario){
-    //     //validações
-    //     Result resultado;
-    //     if(usuario.getId() != 0)
-    //         resultado = dao.editar(usuario);
-    //     else
-    //         resultado = dao.cadastrar(usuario);
-
-    //     return resultado;
-    // }
-
-    public Result cadastrar(String nome, LocalDateTime dataNascimento, String email){
+    public Result salvar(Usuario usuario){
         //validação dos dados
-        // nome não pode ser vazio, apenas letras, 
+        if(usuario.getNome().isBlank() || !usuario.getNome().matches("^[a-zA-Z]*$")){
+            return Result.fail("O nome cadastrado deve possuir apenas letras e não pode estar vazio");
+        }
+        
         //data n pode ser vazia, e nao pode ser futura
+        if(usuario.getDataNascimento() == null || usuario.getDataNascimento().isAfter(LocalDateTime.now())){
+            return Result.fail("A data não pode estar vazia e não pode ser uma data futura!");
+        }
         //email deve conter o @.com  //procurar REGEX (Regular Expression)
+        if(usuario.getEmail().isBlank() || !usuario.getEmail().matches("^[A-Za-z0-9+_\\.\\-]+@(\\.+)$")){
+            return Result.fail("O email deve ser no formato padrão. Ex: exemplo@email.com");
+        }
         
-        Usuario usuario = new Usuario(nome, dataNascimento, email);
-        return dao.cadastrar(usuario);
+        if(usuario.getId() != 0)
+            return dao.editar(usuario);
+        else
+            return dao.cadastrar(usuario);
     }
-    
 
+    public Usuario buscar(String email){    //apenas adm
+        if(!email.isBlank()){
+            return null;
+        }
+        return dao.buscar(email);
 
-    public List<Usuario> listar(){
-        
-        return lista;
+    }
+
+    public List<Usuario> listar(){          //apenas adm
+        return dao.listar();
+    }
+
+    public Result excluir(Usuario usuario){ //apenas adm
+        return dao.excluir(usuario);
     }
 }
